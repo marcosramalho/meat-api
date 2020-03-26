766adc44dfa9766268503a254246d1413b2c1ec5
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-03-26 11:11:04 -0400

    feat: video 3 - req, res, next restify

--- main.ts
+++ main.ts
@@ -5,10 +5,29 @@ const server = restify.createServer({
   version: "0.1.0"
 });
 
-server.get('/', (req, res, next) => {
-  res.json({message: 'hello'});
+server.use(restify.plugins.queryParser());
+
+server.get('/info', [
+(req, res, next) => {
+  if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {    
+    let error: any = new Error();
+    error.statusCode = 400;
+    error.message = "Please, update your browser";
+
+    return next(error);
+  }
+  
   return next();
-})
+}, (req, res, next) => {
+  res.json({ 
+    browser: req.userAgent(),
+    method: req.method,
+    url: req.href(),
+    path: req.path(),
+    query: req.query
+  });
+  return next();
+}])
 
 server.listen(3000, () => {
   console.log('API is running on http://localhost:3000');
