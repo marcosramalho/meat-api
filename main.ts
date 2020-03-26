cb7ec5a46bdb68a9b1a9f336be3d1885fbd8af64
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-03-26 10:55:54 -0400

    feat: video 2 - add hello world with restify

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log('main script 2.....');
+import * as restify from 'restify';
+
+const server = restify.createServer({
+  name: "meat-api",
+  version: "0.1.0"
+});
+
+server.get('/', (req, res, next) => {
+  res.json({message: 'hello'});
+  return next();
+})
+
+server.listen(3000, () => {
+  console.log('API is running on http://localhost:3000');
+});
