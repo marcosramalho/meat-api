cfbfdcbaf5deabd51f694e639ee70d51c6fd5b04
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-03-27 02:05:29 -0400

    feat: add validators erros mongoose

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -1,7 +1,7 @@
 import * as restify from 'restify'
 
 export const handleError = (req: restify.Request, res: restify.Response, err, done) =>{
-  
+    
   err.toJSON = () => {    
     return {
       message: err.message
@@ -16,6 +16,14 @@ export const handleError = (req: restify.Request, res: restify.Response, err, do
       break;
     case 'ValidationError':
       err.statusCode = 400
+      const messages: any[] = []
+      for (let name in err.errors) {
+        messages.push({ message: err.errors[name].message})
+      }
+
+      err.toJSON = () => ({
+        errors: messages
+      })
       break;
   }
 
--- users/users.model.ts
+++ users/users.model.ts
@@ -9,14 +9,25 @@ export interface User extends mongoose.Document {
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
+    maxlength: 80,
+    minlength: 3
   },
   email: {
     type: String,
     unique: true,
+    required: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
   password: {
     type: String, 
-    select: false   
+    select: false,
+    required: true   
+  },
+  gender: {
+    type: String,
+    required: false,
+    enum: ["Male", "Female"]
   }
 })
 
