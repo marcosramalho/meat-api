3c7f10c175a33bf417c7b457c6fc2b6fb74d92b0
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-03-27 01:57:41 -0400

    chore: add restify-errors and handler errors application

--- users/users.router.ts
+++ users/users.router.ts
@@ -2,6 +2,7 @@ import * as restify from 'restify';
 import * as mongoose from 'mongoose';
 import { Router } from "../common/router";
 import { User } from './users.model';
+import { NotFoundError } from 'restify-errors';
 
 
 class UsersRouter extends Router {
@@ -16,16 +17,16 @@ class UsersRouter extends Router {
 
   applyRoutes(application: restify.Server) {
     application.get('/users', (req, res, next) => {
-      User.find().then(this.render(res, next))
+      User.find().then(this.render(res, next)).catch(next)
     })
 
     application.get('/users/:id', (req, res, next) => {
-      User.findById(req.params.id).then(this.render(res, next))
+      User.findById(req.params.id).then(this.render(res, next)).catch(next)
     })
 
     application.post('/users', (req, res, next) => {
       let user = new User(req.body);      
-      user.save().then(this.render(res, next))
+      user.save().then(this.render(res, next)).catch(next)
     })
 
     application.put('/users/:id', (req, res, next) => {
@@ -34,14 +35,14 @@ class UsersRouter extends Router {
         if ( result.n ) {
           return User.findById(req.params.id)
         } else {
-          res.send(404)          
+          throw new NotFoundError('Documento não encontrado')        
         }
-      }).then(this.render(res, next))
+      }).then(this.render(res, next)).catch(next)
     })
 
     application.patch('/users/:id', (req, res, next) => {
       const options = { new: true }
-      User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next))
+      User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next)).catch(next)
     })
 
     application.del('/users/:id', (req, res, next) => {
@@ -49,11 +50,11 @@ class UsersRouter extends Router {
         if (result.result.n) {
           res.send(204)          
         } else {
-          res.send(404)
+          throw new NotFoundError('Documento não encontrado')
         }        
 
         return next()
-      })
+      }).catch(next)
     })
   }
 }
