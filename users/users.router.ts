833d093b27d6687a3201777da7957af0687757d1
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-03-28 01:27:43 -0400

    refactor: add model-router

--- users/users.router.ts
+++ users/users.router.ts
@@ -3,12 +3,12 @@ import * as mongoose from 'mongoose';
 import { Router } from "../common/router";
 import { User } from './users.model';
 import { NotFoundError } from 'restify-errors';
+import { ModelRouter } from '../common/model-router';
 
-
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
 
   constructor() {
-    super();
+    super(User);
     this.on('beforeRender', document => {
       document.password = undefined
       //delete coument.password
@@ -16,46 +16,12 @@ class UsersRouter extends Router {
   }
 
   applyRoutes(application: restify.Server) {
-    application.get('/users', (req, res, next) => {
-      User.find().then(this.render(res, next)).catch(next)
-    })
-
-    application.get('/users/:id', (req, res, next) => {
-      User.findById(req.params.id).then(this.render(res, next)).catch(next)
-    })
-
-    application.post('/users', (req, res, next) => {
-      let user = new User(req.body);      
-      user.save().then(this.render(res, next)).catch(next)
-    })
-
-    application.put('/users/:id', (req, res, next) => {
-      const options = { runValidators: true, overwrite: true }
-      User.update({ _id: req.params.id}, req.body, options).exec().then(result => {
-        if ( result.n ) {
-          return User.findById(req.params.id)
-        } else {
-          throw new NotFoundError('Documento não encontrado')        
-        }
-      }).then(this.render(res, next)).catch(next)
-    })
-
-    application.patch('/users/:id', (req, res, next) => {      
-      const options = { runValidators: true, new: true }
-      User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next)).catch(next)
-    })
-
-    application.del('/users/:id', (req, res, next) => {
-      User.remove({_id: req.params.id}).exec().then((result: any) => {
-        if (result.result.n) {
-          res.send(204)          
-        } else {
-          throw new NotFoundError('Documento não encontrado')
-        }        
-
-        return next()
-      }).catch(next)
-    })
+    application.get('/users', this.findAll)
+    application.get('/users/:id', [this.validateId, this.findById])
+    application.post('/users', this.save)
+    application.put('/users/:id', [this.validateId, this.replace])
+    application.patch('/users/:id', [this.validateId, this.update])
+    application.del('/users/:id', [this.validateId, this.delete])
   }
 }
 
