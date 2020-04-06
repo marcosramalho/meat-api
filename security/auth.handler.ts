5f552ea49c805521aa8841727edb8e09a2b4d37a
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-04-06 02:46:42 -0400

    chore: add jsonwebtoken and method authenticate user

---
+++ security/auth.handler.ts
@@ -0,0 +1,25 @@
+import * as restify from 'restify'
+import * as jwt from 'jsonwebtoken'
+
+import { User } from './../users/users.model';
+import { NotAuthorizedError } from 'restify-errors';
+import { environment } from '../common/environment';
+
+export const authenticate: restify.RequestHandler = (req, res, next) => {
+  const { email, password } = req.body
+  
+  User.findByEmail(email, '+password').then(user => {
+    if (user && user.matches(password)) {
+      // gerar token
+      const token = jwt.sign({
+        sub: user.email, 
+        iss: 'meat-api',      
+      }, environment.security.apiSecret)
+
+      res.json({name: user.name, email: user.email, acessToken: token})
+      return next(false)
+    } else {
+      return next(new NotAuthorizedError('Invalid Credentials'))
+    }
+  }).catch(next) 
+}
