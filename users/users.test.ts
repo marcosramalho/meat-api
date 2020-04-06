e1d157d82ab2938842e1cc4c224a46e318538e58
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-04-05 20:58:31 -0400

    feat: add test GET /user/:id - not found and test PATCH /users

--- users/users.test.ts
+++ users/users.test.ts
@@ -42,6 +42,30 @@ test('POST /users', () => {
   }).catch(fail)
 })
 
+test('GET /users/aaaa - not found', () => {
+  return request(address).get('/users/aaaaaa')
+  .then(response => {
+    expect(response.status).toBe(404)    
+  }).catch(fail)
+})
+
+test('patch /users/:id', () => {
+  return request(address).post('/users')
+  .send({
+    name: 'usuario2',
+    email: 'usuario2@gmail.com',
+    password: '123456',    
+  }).then(response => request(address).patch(`/users/${response.body._id}`).send({
+    name: 'usuario2 - patch'
+  })).then(response => {
+    expect(response.status).toBe(200)
+    expect(response.body._id).toBeDefined()
+    expect(response.body.name).toBe('usuario2 - patch')
+    expect(response.body.email).toBe('usuario2@gmail.com')    
+    expect(response.body.password).toBeUndefined()
+  }).catch(fail)
+})
+
 afterAll(() => {
   return server.shutdown()
 })
